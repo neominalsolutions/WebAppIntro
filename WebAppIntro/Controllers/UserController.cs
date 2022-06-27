6689c8c41cc27fe59a4f68e27f86422e7a6cf677
mert.alptekin <mert.alptekin@neomina.com>
2022-06-27 22:25:25 +0300

    Add project files.

---
+++ WebAppIntro/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppIntro.Models;
+
+namespace WebAppIntro.Controllers
+{
+  public class UserController : Controller
+  {
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+      var data = new List<User>();
+      data.Add(new User { Name = "Ali", SurName = "Tan" });
+      data.Add(new User { Name = "Ayşe", SurName = "Can" });
+
+      return View(data);
+    }
+
+    // Bu web sayfasının açılması o ekranın tarayıcıda görütüntülenmesi için gerekli olan bir http isteği
+    // mvc bir actionresult üstüne açılmayan bir httpverb attribute de http get olarak davranır.
+    [HttpGet] // attribute
+    public IActionResult Create(int? id, string? name)
+    {
+      return View();
+    }
+
+    // Sayfa açmak ama<çlı değil yani sayfa listeleme amaçlı olmayıp sayfadaki formu web sunucunda göndermek için gerekli olan bir fiil (httpverb)
+    [HttpPost]
+    public IActionResult Create(User model)
+    {
+
+      if (ModelState.IsValid)
+      {
+        // veri tabanı işlemi yap
+      }
+
+
+      return View();
+    }
+
+  }
+}
---
+++ WebAppIntro/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppIntro.Models
+{
+  public class User
+  {
+
+    /// <summary>
+    /// attributed based validation
+    /// </summary>
+    [Required(ErrorMessage ="Name alanı boş geçilemez")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage ="Soyad alanı boş geçilemez")]
+    [StringLength(12,ErrorMessage = "Soyad alanı 12 karakteri aşamaz")]
+    public string SurName { get; set; }
+
+  }
+}
